27a2ef6b88d3cb71bdeabf35b6e111003f50bb68
Jae No <jaeyno@gmail.com>
2021-02-15 22:08:32 -0500

    <Added home and register component>

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  registerMode = false;
+
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(e) {
+    this.registerMode = e
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(users => {
+      this.users = users;
+    })
+  }
+}
