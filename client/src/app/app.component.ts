84faacf1d96bdd7d45b51472aed40d86a22c79e1
Jae No <jaeyno@gmail.com>
2021-02-15 21:25:08 -0500

    <keep the status of logged-in> used observable to persist the login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_model/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit{
+  title = 'The Dating App';
+  users: any;
+
+  constructor(private http: HttpClient, private accountService: AccountService) {}
+
+  ngOnInit() {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
 }
