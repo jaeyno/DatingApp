ddc8143c8fec667845a37e7421dabd5ab594ff6d
Jae No <jaeyno@gmail.com>
2021-02-19 11:09:50 -0500

    <crated a member.service.ts> it will get all the members and individual member

---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,28 @@
+import { Member } from './../_model/member';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { Injectable } from '@angular/core';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users', httpOptions);
+  }
+
+  getMember(username: string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username, httpOptions);
+  }
+}
