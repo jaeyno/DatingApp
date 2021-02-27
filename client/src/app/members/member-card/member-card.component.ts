586ec57fa8de48c32654a53fb4c97b631b6b9897
Jae No <jaeyno@gmail.com>
2021-02-26 23:05:59 -0500

    <Added likes function to client side>

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { MembersService } from './../../_services/members.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from 'src/app/_model/member';
 
@@ -10,9 +12,15 @@ export class MemberCardComponent implements OnInit {
 
   @Input() member: Member;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  addLike(member: Member) {
+    this.memberService.addLike(member.username).subscribe(() => {
+      this.toastr.success('you have liked ' + member.knownAs);
+    })
   }
 
 }
