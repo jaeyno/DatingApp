fb4ccd2ab832c9402848ef529e010ee31425cc10
Jae No <jaeyno@gmail.com>
2021-02-28 12:02:54 -0500

    <Set up the angular app for messaging feature>

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,3 +1,6 @@
+import { MessageService } from './../_services/message.service';
+import { Pagination } from './../_model/pagination';
+import { Message } from './../_model/message';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  messages: Message[];
+  pagination: Pagination;
+  container = 'Inbox';
+  pageNumber = 1;
+  pageSize = 5;
+
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container)
+      .subscribe(
+        response => {
+          this.messages = response.result;
+          this.pagination =response.pagination;
+      })
+  }
+
+  pageChanged(event: any) {
+    this.pageNumber = event.page;
+    this.loadMessages();
   }
 
 }
