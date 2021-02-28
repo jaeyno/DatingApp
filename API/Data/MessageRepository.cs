b41ee5b583de90590687b25363e0f2401b3e6ae6
Jae No <jaeyno@gmail.com>
2021-02-27 23:46:52 -0500

    <Set up the auto-mapper profile for message properties>

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -22,7 +22,7 @@ namespace API.Data
 
         public void DeleteMessage(Message message)
         {
-            _context.Messages.Remove(message)
+            _context.Messages.Remove(message);
         }
 
         public async Task<Message> GetMessage(int id)
