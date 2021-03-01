bf28c26d6f4dbc35f6b85f027fba83355eb2de93
Jae No <jaeyno@gmail.com>
2021-02-28 20:46:40 -0500

    <Added the message thread to client side>

---
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -0,0 +1,27 @@
+import { MessageService } from './../../_services/message.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { Message } from 'src/app/_model/message';
+
+@Component({
+  selector: 'app-member-messages',
+  templateUrl: './member-messages.component.html',
+  styleUrls: ['./member-messages.component.css']
+})
+export class MemberMessagesComponent implements OnInit {
+
+  @Input() username: string;
+  messages: Message[];
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messageService.getMessageThread(this.username).subscribe(messages => {
+      this.messages = messages;
+    })
+  }
+
+}
