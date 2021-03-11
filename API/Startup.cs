a4d7cfb17e1e4b6ae26ce129e3f415f064d4f698
Jae No <jaeyno@gmail.com>
2021-03-11 14:49:34 -0500

    <Added new url to cors>

--- API/Startup.cs
+++ API/Startup.cs
@@ -66,7 +66,7 @@ namespace API
             app.UseCors(policy => policy.AllowAnyHeader()
                                         .AllowAnyMethod()
                                         .AllowCredentials()
-                                        .WithOrigins("https://localhost:4200"));
+                                        .WithOrigins("https://localhost:4200", "https://publicdatingapp.herokuapp.com/"));
 
             app.UseAuthentication();
 
